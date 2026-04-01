d3f0f7c14055fa3fc5d37f269fba409160b3285c
Scipio01 <68809974+Scipio01@users.noreply.github.com>
2026-04-01 09:39:37 +0100

    Update practice.js

--- js/practice.js
+++ js/practice.js
@@ -263,18 +263,16 @@ for (let i = 0; i < places; i++) {
     currentQuestionType = "overflow";
     currentSourceValue = { binary1, binary2, registerSize, total, overflow };
   
-    currentQuestion = `A ${registerSize}-bit register is used.
-
-    When these binary numbers are added, will overflow occur?
-    
-    <pre class="binary-block">
-      ${binary1}
-    + ${binary2}
-    </pre>
-    
-    (Overflow means the result is too large to fit in ${registerSize} bits)
-    
-    Answer Yes or No.`;
+  currentQuestion = `A ${registerSize}-bit register is used.
+  
+  When these binary numbers are added, will overflow occur?
+  
+  <pre class="binary-block">${binary1}
+  + ${binary2}</pre>
+  
+  (Overflow means the result is too large to fit in ${registerSize} bits)
+  
+  Answer Yes or No.`;
     
   currentAnswer = overflow ? "yes" : "no";
   
