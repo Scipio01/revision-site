6228ee27d9ff13c38606f4ce0ee3120ec667dd7f
Scipio01 <68809974+Scipio01@users.noreply.github.com>
2026-04-10 19:14:55 +0100

    Update practice.js

--- js/practice.js
+++ js/practice.js
@@ -569,7 +569,7 @@ if (topic === "pseudocode") {
     }
   ];
 
-  const item = pseudocodeQuestions[Math.floor(Math.random() * pseudocodeQuestions.length)];
+  const item = pseudocodeQuestions[(questionNumber - 1) % pseudocodeQuestions.length];
   currentQuestionType = "pseudocode";
   currentQuestion = item.question;
   currentAnswer = item.answer;
